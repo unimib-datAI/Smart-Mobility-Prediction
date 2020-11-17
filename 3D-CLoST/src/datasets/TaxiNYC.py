Fixed normalization

3D-CLoST/src/datasets/TaxiNYC.py
@@ -9,7 +9,8 @@ import h5py
 
 from ..utils import create_dict, create_mask
 from . import load_stdata, stat
-from ..preprocessing import MinMaxNormalization, timestamp2vec
+from ..preprocessing import timestamp2vec
+from ..preprocessing.minmax_normalization import MinMaxNormalization_01
 from .STMatrix import STMatrix
 
 def remove_incomplete_days(data, timestamps, T=24):
@@ -69,7 +70,7 @@ def load_data(T=24, nb_flow=2, len_closeness=None, len_period=None, len_trend=No
     # minmax_scale
     data_train = np.vstack(copy(data_all))[:-len_test]
     print('train_data shape: ', data_train.shape)
-    mmn = MinMaxNormalization()
+    mmn = MinMaxNormalization_01()
     mmn.fit(data_train)
     data_all_mmn = [mmn.transform(d) for d in data_all]
 
@@ -128,8 +129,8 @@ def load_data(T=24, nb_flow=2, len_closeness=None, len_period=None, len_trend=No
     if meta_data:
         meta_feature = timestamp2vec(timestamps_Y)
         metadata_dim = meta_feature.shape[1]
-        meta_feature_train_all, meta_feature_train, meta_feature_val, meta_feature_test = meta_feature[
-        :-len_test], meta_feature[:-len_val], meta_feature[-len_val:-len_test], meta_feature[-len_test:]
+        meta_feature_train_all =  meta_feature[:-len_test]
+        meta_feature_train, meta_feature_val, meta_feature_test = meta_feature_train_all[:-len_val], meta_feature[-len_val:-len_test], meta_feature[-len_test:]
         X_train_all.append(meta_feature_train_all)  
         X_train.append(meta_feature_train)
         X_val.append(meta_feature_val)