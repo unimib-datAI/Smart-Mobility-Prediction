290c409f05358f73fea8c7e82e19571f43cef809
robyt96 <roby_tagliabue@yahoo.it>
2020-12-04 16:17:28 +0100

    Bayesian optimization taxiNYC+feature esterne

--- STAR/main_taxiNYC.py
+++ STAR/main_taxiNYC.py
@@ -4,11 +4,14 @@ import _pickle as pickle
 import numpy as np
 import math
 import h5py
+import json
+import time
 from sklearn.model_selection import ParameterGrid
 
 from keras import backend as K
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+from bayes_opt import BayesianOptimization
 
 from star.model import *
 import star.metrics as metrics
@@ -24,9 +27,9 @@ T = 24  # number of time intervals in one day
 CACHEDATA = True  # cache data or NOT
 
 lr = [0.00015, 0.00035]  # learning rate
-# len_closeness = 3  # length of closeness dependent sequence
-# len_period = 1  # length of peroid dependent sequence
-# len_trend = 1  # length of trend dependent sequence
+len_c = 3  # length of closeness dependent sequence
+len_p = 1  # length of peroid dependent sequence
+len_t = 1  # length of trend dependent sequence
 len_cpt = [[3,1,1]]
 nb_residual_unit = [2,4,6]   # number of residual units
 
@@ -118,129 +121,275 @@ def cache(fname, X_train_all, Y_train_all, X_train, Y_train, X_val, Y_val, X_tes
     h5.create_dataset('T_test', data=timestamp_test)
     h5.close()
 
-# build grid for grid search
-params = {
-    'len_cpt': len_cpt,
-    'batch_size': batch_size,
-    'lr': lr,
-    'nb_residual_unit': nb_residual_unit
-}
+# # build grid for grid search
+# params = {
+#     'len_cpt': len_cpt,
+#     'batch_size': batch_size,
+#     'lr': lr,
+#     'nb_residual_unit': nb_residual_unit
+# }
 
-grid = ParameterGrid(params)
-# print(grid)
+# grid = ParameterGrid(params)
+# # print(grid)
 
 
-for i in range(len(grid)):
-    # extract current grid params
-    len_c, len_p, len_t = grid[i]['len_cpt']
-    nb_res_unit = grid[i]['nb_residual_unit']
-    lr = grid[i]['lr']
-    batch_size = grid[i]['batch_size']
+# for i in range(len(grid)):
+#     # extract current grid params
+#     len_c, len_p, len_t = grid[i]['len_cpt']
+#     nb_res_unit = grid[i]['nb_residual_unit']
+#     lr = grid[i]['lr']
+#     batch_size = grid[i]['batch_size']
 
-    print('Step [{}/{}], len_c {}, len_p {}, len_t {}, res_unit {}, lr {}, batch_size {}'
-        .format(i+1, len(grid), len_c, len_p, len_t, nb_res_unit, lr, batch_size))
+#     print('Step [{}/{}], len_c {}, len_p {}, len_t {}, res_unit {}, lr {}, batch_size {}'
+#         .format(i+1, len(grid), len_c, len_p, len_t, nb_res_unit, lr, batch_size))
 
-    # load data
-    print("loading data...")
-    fname = os.path.join(path_cache, 'TaxiNYC_C{}_P{}_T{}.h5'.format(
-        len_c, len_p, len_t))
-    if os.path.exists(fname) and CACHEDATA:
-        X_train_all, Y_train_all, X_train, Y_train, \
-        X_val, Y_val, X_test, Y_test, mmn, external_dim, \
-        timestamp_train_all, timestamp_train, timestamp_val, timestamp_test = read_cache(
-            fname)
-        print("load %s successfully" % fname)
-    else:
-        X_train_all, Y_train_all, X_train, Y_train, \
-        X_val, Y_val, X_test, Y_test, mmn, external_dim, \
-        timestamp_train_all, timestamp_train, timestamp_val, timestamp_test = TaxiNYC.load_data(
-            T=T, nb_flow=nb_flow, len_closeness=len_c, len_period=len_p, len_trend=len_t, len_test=len_test,
-            len_val=len_val, preprocess_name='preprocessing_taxinyc.pkl', meta_data=True, datapath=DATAPATH)
-        if CACHEDATA:
-            cache(fname, X_train_all, Y_train_all, X_train, Y_train, X_val, Y_val, X_test, Y_test,
-                  external_dim, timestamp_train_all, timestamp_train, timestamp_val, timestamp_test)
+#     # load data
+#     print("loading data...")
+#     fname = os.path.join(path_cache, 'TaxiNYC_C{}_P{}_T{}.h5'.format(
+#         len_c, len_p, len_t))
+#     if os.path.exists(fname) and CACHEDATA:
+#         X_train_all, Y_train_all, X_train, Y_train, \
+#         X_val, Y_val, X_test, Y_test, mmn, external_dim, \
+#         timestamp_train_all, timestamp_train, timestamp_val, timestamp_test = read_cache(
+#             fname)
+#         print("load %s successfully" % fname)
+#     else:
+#         X_train_all, Y_train_all, X_train, Y_train, \
+#         X_val, Y_val, X_test, Y_test, mmn, external_dim, \
+#         timestamp_train_all, timestamp_train, timestamp_val, timestamp_test = TaxiNYC.load_data(
+#             T=T, nb_flow=nb_flow, len_closeness=len_c, len_period=len_p, len_trend=len_t, len_test=len_test,
+#             len_val=len_val, preprocess_name='preprocessing_taxinyc.pkl', meta_data=True, datapath=DATAPATH)
+#         if CACHEDATA:
+#             cache(fname, X_train_all, Y_train_all, X_train, Y_train, X_val, Y_val, X_test, Y_test,
+#                   external_dim, timestamp_train_all, timestamp_train, timestamp_val, timestamp_test)
 
-    # print("\n days (test): ", [v[:8] for v in timestamp_test[0::T]])
-    print('=' * 10)
+#     # print("\n days (test): ", [v[:8] for v in timestamp_test[0::T]])
+#     print('=' * 10)
 
-    iterations = 1
-    for iteration in range(0, iterations):
-        # build model
-        print(f'Iteration {iteration}')
-        tf.keras.backend.set_image_data_format('channels_first')
+#     iterations = 1
+#     for iteration in range(0, iterations):
+#         # build model
+#         print(f'Iteration {iteration}')
+#         tf.keras.backend.set_image_data_format('channels_first')
 
-        model = build_model(len_c, len_p, len_t, nb_flow, map_height,
-                            map_width, external_dim, nb_res_unit,
-                            bn=True,
-                            bn2=True,
-                            save_model_pic=False,
-                            lr=lr
-                            )
+#         model = build_model(len_c, len_p, len_t, nb_flow, map_height,
+#                             map_width, external_dim, nb_res_unit,
+#                             bn=True,
+#                             bn2=True,
+#                             save_model_pic=False,
+#                             lr=lr
+#                             )
 
-        hyperparams_name = 'TaxiNYC.c{}.p{}.t{}.resunit_{}.lr_{}.batchsize_{}.iter{}'.format(
-            len_c, len_p, len_t, nb_res_unit, lr, batch_size, iteration)
-        fname_param = os.path.join(path_model, '{}.best.h5'.format(hyperparams_name))
-        print(hyperparams_name)
+#         hyperparams_name = 'TaxiNYC.c{}.p{}.t{}.resunit_{}.lr_{}.batchsize_{}.iter{}'.format(
+#             len_c, len_p, len_t, nb_res_unit, lr, batch_size, iteration)
+#         fname_param = os.path.join(path_model, '{}.best.h5'.format(hyperparams_name))
+#         print(hyperparams_name)
 
-        early_stopping = EarlyStopping(monitor='val_rmse', patience=25, mode='min')
-        model_checkpoint = ModelCheckpoint(
-            fname_param, monitor='val_rmse', verbose=0, save_best_only=True, mode='min')
+#         early_stopping = EarlyStopping(monitor='val_rmse', patience=25, mode='min')
+#         model_checkpoint = ModelCheckpoint(
+#             fname_param, monitor='val_rmse', verbose=0, save_best_only=True, mode='min')
 
-        print('=' * 10)
-        # train model
-        np.random.seed(i*18)
-        tf.random.set_seed(i*18)
-        print("training model...")
-        history = model.fit(X_train, Y_train,
-                            epochs=nb_epoch,
-                            batch_size=batch_size,
-                            validation_data=(X_val,Y_val),
-                            callbacks=[early_stopping, model_checkpoint],
-                            verbose=0)
-        model.save_weights(os.path.join(
-            path_model, '{}.h5'.format(hyperparams_name)), overwrite=True)
-        pickle.dump((history.history), open(os.path.join(
-            path_result, '{}.history.pkl'.format(hyperparams_name)), 'wb'))
+#         print('=' * 10)
+#         # train model
+#         np.random.seed(i*18)
+#         tf.random.set_seed(i*18)
+#         print("training model...")
+#         history = model.fit(X_train, Y_train,
+#                             epochs=nb_epoch,
+#                             batch_size=batch_size,
+#                             validation_data=(X_val,Y_val),
+#                             callbacks=[early_stopping, model_checkpoint],
+#                             verbose=0)
+#         model.save_weights(os.path.join(
+#             path_model, '{}.h5'.format(hyperparams_name)), overwrite=True)
+#         pickle.dump((history.history), open(os.path.join(
+#             path_result, '{}.history.pkl'.format(hyperparams_name)), 'wb'))
 
-        print('=' * 10)
+#         print('=' * 10)
 
-        # evaluate model
-        print('evaluating using the model that has the best loss on the valid set')
-        model.load_weights(fname_param) # load best weights for current iteration
+#         # evaluate model
+#         print('evaluating using the model that has the best loss on the valid set')
+#         model.load_weights(fname_param) # load best weights for current iteration
         
-        Y_pred = model.predict(X_test) # compute predictions
+#         Y_pred = model.predict(X_test) # compute predictions
 
-        score = evaluate(Y_test, Y_pred, mmn, rmse_factor=1) # evaluate performance
+#         score = evaluate(Y_test, Y_pred, mmn, rmse_factor=1) # evaluate performance
+
+#         # save to csv
+#         csv_name = os.path.join('results','star_bikeNYC_results.csv')
+#         if not os.path.isfile(csv_name):
+#             if os.path.isdir('results') is False:
+#                 os.mkdir('results')
+#             with open(csv_name, 'a', encoding = "utf-8") as file:
+#                 file.write(
+#                   'len_closeness,len_period,len_trend,'
+#                   'nb_residual_unit,'
+#                   'learning rate,'
+#                   'batch_size,'
+#                   'iteration,'
+#                   'rsme_in,rsme_out,rsme_tot,'
+#                   'mape_in,mape_out,mape_tot,'
+#                   'ape_in,ape_out,ape_tot'
+#                 )
+#                 file.write("\n")
+#                 file.close()
+#         with open(csv_name, 'a', encoding = "utf-8") as file:
+#             file.write(
+#               f'{len_c},{len_p},{len_t},'
+#               f'{nb_res_unit},'
+#               f'{lr},'
+#               f'{batch_size},'
+#               f'{iteration},'
+#               f'{score[0]},{score[1]},{score[2]},'
+#               f'{score[3]},{score[4]},{score[5]},'
+#               f'{score[6]},{score[7]},{score[8]}'
+#             )
+#             file.write("\n")
+#             file.close()
+#         K.clear_session()
+
+print("loading data...")
+fname = os.path.join(path_cache, 'TaxiNYC_C{}_P{}_T{}.h5'.format(
+    len_c, len_p, len_t))
+if os.path.exists(fname) and CACHEDATA:
+    X_train_all, Y_train_all, X_train, Y_train, \
+    X_val, Y_val, X_test, Y_test, mmn, external_dim, \
+    timestamp_train_all, timestamp_train, timestamp_val, timestamp_test = read_cache(
+        fname)
+    print("load %s successfully" % fname)
+else:
+    X_train_all, Y_train_all, X_train, Y_train, \
+    X_val, Y_val, X_test, Y_test, mmn, external_dim, \
+    timestamp_train_all, timestamp_train, timestamp_val, timestamp_test = TaxiNYC.load_data(
+        T=T, nb_flow=nb_flow, len_closeness=len_c, len_period=len_p, len_trend=len_t, len_test=len_test,
+        len_val=len_val, preprocess_name='preprocessing_taxinyc.pkl', meta_data=True,
+        meteorol_data=True, holiday_data=True, datapath=DATAPATH)
+    if CACHEDATA:
+        cache(fname, X_train_all, Y_train_all, X_train, Y_train, X_val, Y_val, X_test, Y_test,
+                external_dim, timestamp_train_all, timestamp_train, timestamp_val, timestamp_test)
+
+print("\n days (test): ", [v[:8] for v in timestamp_test[0::T]])
+print('=' * 10)
+
+def train_model(lr, batch_size, residual_units, save_results=False, i=''):
+    # get discrete parameters
+    residual_units = int(residual_units) * 2
+    batch_size = 16 * int(batch_size)
+    # kernel_size = int(kernel_size)
+    lr = round(lr,5)
+
+    # build model
+    tf.keras.backend.set_image_data_format('channels_first')
+    model = build_model(len_c, len_p, len_t, nb_flow, map_height,
+                        map_width, external_dim, residual_units,
+                        bn=True,
+                        bn2=True,
+                        save_model_pic=False,
+                        lr=lr
+                        )
+    # model.summary()
+    hyperparams_name = 'TaxiNYC{}.c{}.p{}.t{}.resunits_{}.lr_{}.batchsize_{}'.format(
+        i, len_c, len_p, len_t, residual_units,
+        lr, batch_size)
+    fname_param = os.path.join('MODEL', '{}.best.h5'.format(hyperparams_name))
+
+    early_stopping = EarlyStopping(monitor='val_rmse', patience=25, mode='min')
+    # lr_callback = LearningRateScheduler(lrschedule)
+    model_checkpoint = ModelCheckpoint(
+        fname_param, monitor='val_rmse', verbose=0, save_best_only=True, mode='min')
+
+    # train model
+    print("training model...")
+    ts = time.time()
+    if (i):
+        print(f'Iteration {i}')
+        np.random.seed(i * 18)
+        tf.random.set_seed(i * 18)
+    history = model.fit(X_train_all, Y_train_all,
+                        epochs=nb_epoch,
+                        batch_size=batch_size,
+                        validation_data=(X_test, Y_test),
+                        # callbacks=[early_stopping, model_checkpoint],
+                        # callbacks=[model_checkpoint, lr_callback],
+                        callbacks=[model_checkpoint],
+                        verbose=2)
+    model.save_weights(os.path.join(
+        'MODEL', '{}.h5'.format(hyperparams_name)), overwrite=True)
+    pickle.dump((history.history), open(os.path.join(
+        path_result, '{}.history.pkl'.format(hyperparams_name)), 'wb'))
+    print("\nelapsed time (training): %.3f seconds\n" % (time.time() - ts))
+
+    # evaluate
+    model.load_weights(fname_param)
+    score = model.evaluate(
+        X_test, Y_test, batch_size=Y_test.shape[0], verbose=0)
+    print('Test score: %.6f rmse (norm): %.6f rmse (real): %.6f' %
+          (score[0], score[1], score[1] * (mmn._max - mmn._min) / 2.))
+
+    if (save_results):
+        print('evaluating using the model that has the best loss on the valid set')
+        model.load_weights(fname_param)  # load best weights for current iteration
+
+        Y_pred = model.predict(X_test)  # compute predictions
+
+        score = evaluate(Y_test, Y_pred, mmn, rmse_factor=1)  # evaluate performance
 
         # save to csv
-        csv_name = os.path.join('results','star_bikeNYC_results.csv')
+        csv_name = os.path.join('results', 'star_taxiNYC_results.csv')
         if not os.path.isfile(csv_name):
             if os.path.isdir('results') is False:
                 os.mkdir('results')
-            with open(csv_name, 'a', encoding = "utf-8") as file:
-                file.write(
-                  'len_closeness,len_period,len_trend,'
-                  'nb_residual_unit,'
-                  'learning rate,'
-                  'batch_size,'
-                  'iteration,'
-                  'rsme_in,rsme_out,rsme_tot,'
-                  'mape_in,mape_out,mape_tot,'
-                  'ape_in,ape_out,ape_tot'
-                )
+            with open(csv_name, 'a', encoding="utf-8") as file:
+                file.write('iteration,'
+                           'rsme_in,rsme_out,rsme_tot,'
+                           'mape_in,mape_out,mape_tot,'
+                           'ape_in,ape_out,ape_tot'
+                           )
                 file.write("\n")
                 file.close()
-        with open(csv_name, 'a', encoding = "utf-8") as file:
-            file.write(
-              f'{len_c},{len_p},{len_t},'
-              f'{nb_res_unit},'
-              f'{lr},'
-              f'{batch_size},'
-              f'{iteration},'
-              f'{score[0]},{score[1]},{score[2]},'
-              f'{score[3]},{score[4]},{score[5]},'
-              f'{score[6]},{score[7]},{score[8]}'
-            )
+        with open(csv_name, 'a', encoding="utf-8") as file:
+            file.write(f'{i},{score[0]},{score[1]},{score[2]},{score[3]},'
+                       f'{score[4]},{score[5]},{score[6]},{score[7]},{score[8]}'
+                       )
             file.write("\n")
             file.close()
         K.clear_session()
+
+    # bayes opt is a maximization algorithm, to minimize validation_loss, return 1-this
+    bayes_opt_score = 1.0 - score[1]
+
+    return bayes_opt_score
+
+# bayesian optimization
+optimizer = BayesianOptimization(f=train_model,
+                                 pbounds={'residual_units': (1, 3.999), # *2
+                                          'lr': (0.001, 0.0001),
+                                          'batch_size': (1, 2.999), # *16
+                                        #   'kernel_size': (3, 5.999)
+                                 },
+                                 verbose=2)
+
+optimizer.maximize(init_points=2, n_iter=10)
+
+# training-test-evaluation iterations with best params
+if os.path.isdir('results') is False:
+    os.mkdir('results')
+targets = [e['target'] for e in optimizer.res]
+bs_fname = 'bs_taxiNYC.json'
+with open(os.path.join('results', bs_fname), 'w') as f:
+    json.dump(optimizer.res, f, indent=2)
+best_index = targets.index(max(targets))
+params = optimizer.res[best_index]['params']
+# save best params
+params_fname = 'star_taxiNYC_best_params.json'
+with open(os.path.join('results', params_fname), 'w') as f:
+    json.dump(params, f, indent=2)
+# with open(os.path.join('results', params_fname), 'r') as f:
+#     params = json.load(f)
+for i in range(0, 10):
+    train_model(residual_units=params['residual_units'],
+                lr=params['lr'],
+                batch_size=params['batch_size'],
+                # kernel_size=params['kernel_size'],
+                save_results=True,
+                i=i)
--- STAR/star/TaxiNYC.py
+++ STAR/star/TaxiNYC.py
@@ -3,11 +3,75 @@ from __future__ import print_function
 import os
 import _pickle as pickle
 import numpy as np
+import h5py
 from star import *
 from star.minmax_normalization import MinMaxNormalization
 from star.STMatrix import STMatrix
 
-np.random.seed(1337)  # for reproducibility
+def load_holiday(timeslots, datapath):
+    fname=os.path.join(datapath, 'TaxiNYC', 'NY_Holiday.txt')
+    f = open(fname, 'r')
+    holidays = f.readlines()
+    holidays = set([h.strip() for h in holidays])
+    H = np.zeros(len(timeslots))
+    for i, slot in enumerate(timeslots):
+        if slot[:8] in holidays:
+            H[i] = 1
+    print(H.sum())
+    # print(timeslots[H==1])
+    return H[:, None]
+
+
+def load_meteorol(timeslots, datapath):
+    '''
+    timeslots: the predicted timeslots
+    In real-world, we dont have the meteorol data in the predicted timeslot, instead,
+    we use the meteoral at previous timeslots, i.e., slot = predicted_slot - timeslot (you can use predicted meteorol data as well)
+    '''
+    def adjust_timeslots(timeslot):
+        timeslot_str = timeslot.decode("utf-8")
+        interval = timeslot_str[-2:]
+        new_interval = f'{int(interval)+1:02}'
+        return bytes(timeslot_str[:-2] + new_interval, encoding='utf8')
+    timeslots = [adjust_timeslots(t) for t in timeslots]
+
+    fname=os.path.join(datapath, 'TaxiNYC', 'NY_Meteorology.h5')
+    f = h5py.File(fname, 'r')
+    Timeslot = f['date'].value
+    WindSpeed = f['WindSpeed'].value
+    Weather = f['Weather'].value
+    Temperature = f['Temperature'].value
+    f.close()
+
+    M = dict()  # map timeslot to index
+    for i, slot in enumerate(Timeslot):
+        M[slot] = i
+
+    WS = []  # WindSpeed
+    WR = []  # Weather
+    TE = []  # Temperature
+    for slot in timeslots:
+        predicted_id = M[slot]
+        cur_id = predicted_id - 1
+        WS.append(WindSpeed[cur_id])
+        WR.append(Weather[cur_id])
+        TE.append(Temperature[cur_id])
+
+    WS = np.asarray(WS)
+    WR = np.asarray(WR)
+    TE = np.asarray(TE)
+
+    # 0-1 scale
+    WS = 1. * (WS - WS.min()) / (WS.max() - WS.min())
+    TE = 1. * (TE - TE.min()) / (TE.max() - TE.min())
+
+    print("shape: ", WS.shape, WR.shape, TE.shape)
+
+    # concatenate all these attributes
+    merge_data = np.hstack([WR, WS[:, None], TE[:, None]])
+
+    # print('meger shape:', merge_data.shape)
+    return merge_data
 
 def remove_incomplete_days(data, timestamps, T=24):
     # remove a certain day which has not T timestamps
@@ -34,7 +98,9 @@ def remove_incomplete_days(data, timestamps, T=24):
     timestamps = [timestamps[i] for i in idx]
     return data, timestamps
 
-def load_data(T=24, nb_flow=2, len_closeness=None, len_period=None, len_trend=None, len_test=None, len_val=None, preprocess_name='preprocessing_taxinyc.pkl', meta_data=True, datapath=None):
+def load_data(T=24, nb_flow=2, len_closeness=None, len_period=None, len_trend=None,
+              len_test=None, len_val=None, preprocess_name='preprocessing_taxinyc.pkl',
+              meta_data=True, meteorol_data=False, holiday_data=False, datapath=None):
     assert(len_closeness + len_period + len_trend > 0)
     # load data
     # 10 - 14
@@ -122,7 +188,21 @@ def load_data(T=24, nb_flow=2, len_closeness=None, len_period=None, len_trend=No
     # print('train shape:', XC_train.shape, Y_train.shape, 'test shape: ', XC_test.shape, Y_test.shape)
     # load meta feature
     if meta_data:
-        meta_feature = timestamp2vec(timestamps_Y)
+        time_feature = timestamp2vec(timestamps_Y)
+        meta_feature.append(time_feature)
+        if holiday_data:
+            # load holiday
+            holiday_feature = load_holiday(timestamps_Y, datapath)
+            meta_feature.append(holiday_feature)
+        if meteorol_data:
+            # load meteorol data
+            meteorol_feature = load_meteorol(timestamps_Y, datapath)
+            meta_feature.append(meteorol_feature)
+        
+        meta_feature = np.hstack(meta_feature) if len(
+            meta_feature) > 0 else np.asarray(meta_feature)
+        metadata_dim = meta_feature.shape[1] if len(
+            meta_feature.shape) > 1 else None
         metadata_dim = meta_feature.shape[1]
         meta_feature_train_all, meta_feature_train, meta_feature_val, meta_feature_test = meta_feature[
         :-len_test], meta_feature[:-len_val], meta_feature[-len_val:-len_test], meta_feature[-len_test:]
