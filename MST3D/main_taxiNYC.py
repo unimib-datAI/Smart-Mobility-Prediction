7f25c0e914a71e02812ac64f057837e69c772297
robyt96 <roby_tagliabue@yahoo.it>
2020-12-09 16:51:18 +0100

    Aggiunti file per otimizzazione taxiNYC

--- MST3D/main_taxiNYC.py
+++ MST3D/main_taxiNYC.py
@@ -6,6 +6,9 @@ import time
 import numpy as np
 import h5py
 import math
+import json
+import time
+from bayes_opt import BayesianOptimization
 from sklearn.model_selection import ParameterGrid
 
 import tensorflow as tf
@@ -27,15 +30,15 @@ nb_epoch = 100  # number of epoch at training stage
 batch_size = [16, 32, 64]  # batch size
 T = 24  # number of time intervals in one day
 lr = [0.00015, 0.00035]  # learning rate
-# len_closeness = 4  # length of closeness dependent sequence - should be 6
-# len_period = 4  # length of peroid dependent sequence
-# len_trend = 4  # length of trend dependent sequence
+len_closeness = len_c = 4  # length of closeness dependent sequence - should be 6
+len_period = len_p = 4  # length of peroid dependent sequence
+len_trend = len_t = 4  # length of trend dependent sequence
 len_cpt = [[4,4,4]]
 nb_flow = 2  # there are two types of flows: inflow and outflow
 
 # divide data into two subsets: Train & Test, of which the test set is the
-# last 10 days
-days_test = 10
+# last 4 weeks
+days_test = 7*4
 len_test = T * days_test
 map_height, map_width = 16, 8  # grid size
 
@@ -99,120 +102,145 @@ def cache(fname, X_train, Y_train, X_test, Y_test, external_dim, timestamp_train
     h5.create_dataset('T_test', data=timestamp_test)
     h5.close()
 
-# build grid for grid search
-params = {
-    'len_cpt': len_cpt,
-    'batch_size': batch_size,
-    'lr': lr,
-}
 
-grid = ParameterGrid(params)
-# print(grid)
+print("loading data...")
+fname = os.path.join(path_cache, 'TaxiNYC_C{}_P{}_T{}.h5'.format(
+    len_c, len_p, len_t))
+if os.path.exists(fname) and CACHEDATA:
+    X_train, Y_train, \
+    X_test, Y_test, mmn, external_dim, \
+    timestamp_train, timestamp_test = read_cache(
+        fname)
+    print("load %s successfully" % fname)
+else:
+    X_train, Y_train, \
+    X_test, Y_test, mmn, external_dim, \
+    timestamp_train, timestamp_test = TaxiNYC.load_data(
+        T=T, nb_flow=nb_flow, len_closeness=len_c, len_period=len_p, len_trend=len_t, len_test=len_test,
+        preprocess_name='preprocessing_taxinyc.pkl', meta_data=True,
+        meteorol_data=True, holiday_data=True, datapath=DATAPATH)
+    if CACHEDATA:
+        cache(fname, X_train, Y_train, X_test, Y_test,
+                external_dim, timestamp_train, timestamp_test)
 
-for i in range(len(grid)):
-    # extract current grid params
-    len_c, len_p, len_t = grid[i]['len_cpt']
-    lr = grid[i]['lr']
-    batch_size = grid[i]['batch_size']
+print("\n days (test): ", [v[:8] for v in timestamp_test[0::T]])
+print('=' * 10)
 
-    print('Step [{}/{}], len_c {}, len_p {}, len_t {}, lr {}, batch_size {}'
-        .format(i+1, len(grid), len_c, len_p, len_t, lr, batch_size))
+def train_model(lr, batch_size, save_results=False, i=''):
+    # get discrete parameters
+    batch_size = 16 * int(batch_size)
+    # kernel_size = int(kernel_size)
+    lr = round(lr,5)
 
-    # load data
-    print("loading data...")
-    fname = os.path.join(path_cache, 'TaxiNYC_C{}_P{}_T{}.h5'.format(
-        len_c, len_p, len_t))
-    if os.path.exists(fname) and CACHEDATA:
-        X_train, Y_train, X_test, Y_test, mmn, external_dim, timestamp_train, timestamp_test = read_cache(
-            fname)
-        print("load %s successfully" % fname)
-    else:
-        X_train, Y_train, X_test, Y_test, mmn, external_dim, timestamp_train, timestamp_test = TaxiNYC.load_data(
-            T=T, nb_flow=nb_flow, len_closeness=len_c, len_period=len_p, len_trend=len_t, len_test=len_test,
-            preprocess_name='preprocessing_taxinyc.pkl', meta_data=True, datapath=DATAPATH)
-        if CACHEDATA:
-            cache(fname, X_train, Y_train, X_test, Y_test,
-                  external_dim, timestamp_train, timestamp_test)
-
-    print('=' * 10)
-
-    # print("\n days (test): ", [v[:8] for v in timestamp_test[0::T]])
-    print('=' * 10)
-
-    iterations = 1
-    for iteration in range(0, iterations):
-        # build model
-        print(f'Iteration {iteration}')
-
-        model = build_model(
+    # build model
+    model = build_model(
             len_c, len_p, len_t, nb_flow, map_height,
             map_width, external_dim,
             save_model_pic=False,
             lr=lr
         )
+    # model.summary()
+    hyperparams_name = 'TaxiNYC{}.c{}.p{}.t{}.lr_{}.batchsize_{}'.format(
+        i, len_c, len_p, len_t,
+        lr, batch_size)
+    fname_param = os.path.join('MODEL', '{}.best.h5'.format(hyperparams_name))
 
-        hyperparams_name = 'TaxiNYC.c{}.p{}.t{}.lr_{}.batchsize_{}.iter{}'.format(
-            len_c, len_p, len_t, lr, batch_size, iteration)
-        fname_param = os.path.join(path_model, '{}.best.h5'.format(hyperparams_name))
-        print(hyperparams_name)
+    early_stopping = EarlyStopping(monitor='val_rmse', patience=25, mode='min')
+    # lr_callback = LearningRateScheduler(lrschedule)
+    model_checkpoint = ModelCheckpoint(
+        fname_param, monitor='val_rmse', verbose=0, save_best_only=True, mode='min')
 
-        early_stopping = EarlyStopping(monitor='val_rmse', patience=25, mode='min')
-        model_checkpoint = ModelCheckpoint(
-            fname_param, monitor='val_rmse', verbose=0, save_best_only=True, mode='min')
+    # train model
+    print("training model...")
+    ts = time.time()
+    if (i):
+        print(f'Iteration {i}')
+        np.random.seed(i * 18)
+        tf.random.set_seed(i * 18)
+    history = model.fit(X_train, Y_train,
+                        epochs=nb_epoch,
+                        batch_size=batch_size,
+                        validation_data=(X_test, Y_test),
+                        # callbacks=[early_stopping, model_checkpoint],
+                        # callbacks=[model_checkpoint, lr_callback],
+                        callbacks=[model_checkpoint],
+                        verbose=2)
+    model.save_weights(os.path.join(
+        'MODEL', '{}.h5'.format(hyperparams_name)), overwrite=True)
+    pickle.dump((history.history), open(os.path.join(
+        path_result, '{}.history.pkl'.format(hyperparams_name)), 'wb'))
+    print("\nelapsed time (training): %.3f seconds\n" % (time.time() - ts))
 
-        print('=' * 10)
-        # train model
-        np.random.seed(i*18)
-        tf.random.set_seed(i*18)
-        print("training model...")
-        history = model.fit(X_train, Y_train,
-                            epochs=nb_epoch,
-                            batch_size=batch_size,
-                            validation_split=0.1,
-                            callbacks=[early_stopping, model_checkpoint],
-                            verbose=0)
-        model.save_weights(os.path.join(
-            path_model, '{}.h5'.format(hyperparams_name)), overwrite=True)
-        pickle.dump((history.history), open(os.path.join(
-            path_result, '{}.history.pkl'.format(hyperparams_name)), 'wb'))
+    # evaluate
+    model.load_weights(fname_param)
+    score = model.evaluate(
+        X_test, Y_test, batch_size=Y_test.shape[0], verbose=0)
+    print('Test score: %.6f rmse (norm): %.6f rmse (real): %.6f' %
+          (score[0], score[1], score[1] * (mmn._max - mmn._min) / 2.))
 
-        print('=' * 10)
-
-        # evaluate model
+    if (save_results):
         print('evaluating using the model that has the best loss on the valid set')
-        model.load_weights(fname_param) # load best weights for current iteration
-        
-        Y_pred = model.predict(X_test) # compute predictions
+        model.load_weights(fname_param)  # load best weights for current iteration
 
-        score = evaluate(Y_test, Y_pred, mmn, rmse_factor=1) # evaluate performance
+        Y_pred = model.predict(X_test)  # compute predictions
+
+        score = evaluate(Y_test, Y_pred, mmn, rmse_factor=1)  # evaluate performance
 
         # save to csv
-        csv_name = os.path.join('results','mst3d_taxiNYC_results.csv')
+        csv_name = os.path.join('results', 'mst3d_taxiNYC_results.csv')
         if not os.path.isfile(csv_name):
             if os.path.isdir('results') is False:
                 os.mkdir('results')
-            with open(csv_name, 'a', encoding = "utf-8") as file:
-                file.write(
-                  'len_closeness,len_period,len_trend,'
-                  'learning rate,'
-                  'batch_size,'
-                  'iteration,'
-                  'rsme_in,rsme_out,rsme_tot,'
-                  'mape_in,mape_out,mape_tot,'
-                  'ape_in,ape_out,ape_tot'
-                )
+            with open(csv_name, 'a', encoding="utf-8") as file:
+                file.write('iteration,'
+                           'rsme_in,rsme_out,rsme_tot,'
+                           'mape_in,mape_out,mape_tot,'
+                           'ape_in,ape_out,ape_tot'
+                           )
                 file.write("\n")
                 file.close()
-        with open(csv_name, 'a', encoding = "utf-8") as file:
-            file.write(
-              f'{len_c},{len_p},{len_t},'
-              f'{lr},'
-              f'{batch_size},'
-              f'{iteration},'
-              f'{score[0]},{score[1]},{score[2]},'
-              f'{score[3]},{score[4]},{score[5]},'
-              f'{score[6]},{score[7]},{score[8]}'
-            )
+        with open(csv_name, 'a', encoding="utf-8") as file:
+            file.write(f'{i},{score[0]},{score[1]},{score[2]},{score[3]},'
+                       f'{score[4]},{score[5]},{score[6]},{score[7]},{score[8]}'
+                       )
             file.write("\n")
             file.close()
         K.clear_session()
+
+    # bayes opt is a maximization algorithm, to minimize validation_loss, return 1-this
+    bayes_opt_score = 1.0 - score[1]
+
+    return bayes_opt_score
+
+# bayesian optimization
+optimizer = BayesianOptimization(f=train_model,
+                                 pbounds={
+                                          'lr': (0.001, 0.0001),
+                                          'batch_size': (1, 3.999), # *16
+                                        #   'kernel_size': (3, 5.999)
+                                 },
+                                 verbose=2)
+
+optimizer.maximize(init_points=2, n_iter=5)
+
+# training-test-evaluation iterations with best params
+if os.path.isdir('results') is False:
+    os.mkdir('results')
+targets = [e['target'] for e in optimizer.res]
+bs_fname = 'bs_taxiNYC.json'
+with open(os.path.join('results', bs_fname), 'w') as f:
+    json.dump(optimizer.res, f, indent=2)
+best_index = targets.index(max(targets))
+params = optimizer.res[best_index]['params']
+# save best params
+params_fname = 'mst3d_taxiNYC_best_params.json'
+with open(os.path.join('results', params_fname), 'w') as f:
+    json.dump(params, f, indent=2)
+# with open(os.path.join('results', params_fname), 'r') as f:
+#     params = json.load(f)
+for i in range(0, 10):
+    train_model(lr=params['lr'],
+                batch_size=params['batch_size'],
+                # kernel_size=params['kernel_size'],
+                save_results=True,
+                i=i)
