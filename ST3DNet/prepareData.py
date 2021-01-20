07563b0e0d96e2a4f362b1148bcc75947e85dea6
stefano <s.fiorini2@campus.unimib.it>
2021-01-20 18:30:34 +0100

    Fix

--- ST3DNet/prepareData.py
+++ ST3DNet/prepareData.py
@@ -1135,37 +1135,37 @@ def load_data_taxiNYC(T=24, nb_flow=2, len_closeness=None, len_period=None, len_
 ###
 
 ### load and cache Roma_Bergamo32x32 data
-DATAPATH = '../data'
-T = 24*2  # number of time intervals in one day
-len_closeness = 6  # length of closeness dependent sequence
-len_period = 0  # length of peroid dependent sequence
-len_trend = 2  # length of trend dependent sequence
-nb_residual_unit = 7   # number of residual units
-nb_flow = 2  # there are two types of flows: new-flow and end-flow
-days_test = 7
-len_test = T * days_test
-map_height, map_width = 32, 32  # grid size
-
-X_train, Y_train, X_test, Y_test, mmn, external_dim, timestamp_train, timestamp_test = \
-        load_data_carRome2(T=T, nb_flow=nb_flow, len_closeness=len_closeness, len_period=len_period,
-                  len_trend=len_trend, len_test=len_test, meta_data=False, meteorol_data=False, holiday_data=True, datapath=DATAPATH)
-
-CACHEDATA=True
-path_cache = os.path.join(DATAPATH, 'CACHE', 'ST3DNet')
-if CACHEDATA and os.path.isdir(path_cache) is False:
-    os.mkdir(path_cache)
-filename = os.path.join(path_cache, 'Rome_c%d_p%d_t%d_noext_2'%(len_closeness, len_period, len_trend))
-
-f = open(filename, 'wb')
-pickle.dump(X_train, f)
-pickle.dump(Y_train, f)
-pickle.dump(X_test, f)
-pickle.dump(Y_test, f)
-pickle.dump(mmn, f)
-pickle.dump(external_dim, f)
-pickle.dump(timestamp_train, f)
-pickle.dump(timestamp_test, f)
-f.close()
+#DATAPATH = '../data'
+#T = 24*2  # number of time intervals in one day
+#len_closeness = 6  # length of closeness dependent sequence
+#len_period = 0  # length of peroid dependent sequence
+#len_trend = 2  # length of trend dependent sequence
+#nb_residual_unit = 7   # number of residual units
+#nb_flow = 2  # there are two types of flows: new-flow and end-flow
+#days_test = 7
+#len_test = T * days_test
+#map_height, map_width = 32, 32  # grid size
+#
+#X_train, Y_train, X_test, Y_test, mmn, external_dim, timestamp_train, timestamp_test = \
+#        load_data_carRome2(T=T, nb_flow=nb_flow, len_closeness=len_closeness, len_period=len_period,
+#                  len_trend=len_trend, len_test=len_test, meta_data=False, meteorol_data=False, holiday_data=True, datapath=DATAPATH)
+#
+#CACHEDATA=True
+#path_cache = os.path.join(DATAPATH, 'CACHE', 'ST3DNet')
+#if CACHEDATA and os.path.isdir(path_cache) is False:
+#    os.mkdir(path_cache)
+#filename = os.path.join(path_cache, 'Rome_c%d_p%d_t%d_noext_2'%(len_closeness, len_period, len_trend))
+#
+#f = open(filename, 'wb')
+#pickle.dump(X_train, f)
+#pickle.dump(Y_train, f)
+#pickle.dump(X_test, f)
+#pickle.dump(Y_test, f)
+#pickle.dump(mmn, f)
+#pickle.dump(external_dim, f)
+#pickle.dump(timestamp_train, f)
+#pickle.dump(timestamp_test, f)
+#f.close()
 ###
 
 ### load and cache RomaNord16x8 data
