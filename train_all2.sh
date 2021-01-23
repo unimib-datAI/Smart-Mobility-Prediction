e5551ca4ec3a56c8706f358c112a50a147afaf6b
stefano <s.fiorini2@campus.unimib.it>
2021-01-23 10:56:36 +0100

    aggiunta bash per transfer learning automatico

---
+++ train_all2.sh
@@ -0,0 +1,33 @@
+# STAR
+cd STAR
+#python main_bikeNYC.py
+python transfer_learning_evaluation.py
+
+# St-ResNet
+cd ../ST-ResNet
+#python main_bikeNYC.py
+python transfer_learning_evaluation.py
+
+# # MST3D
+cd ../MST3D
+python transfer_learning_evaluation.py
+# python main_taxiBJ.py
+
+# # Pred-CNN
+cd ../Pred-CNN
+python transfer_learning_evaluation.py
+# python main_taxiBJ.py
+
+# # ST3DNet
+#cd ../ST3DNet
+#python prepareData.py
+#python transfer_learning_evaluation.py
+# python main_taxiBJ.py
+
+# # 3D-CLoST
+cd ../3D-CLoST
+python transfer_learning_evaluation.py
+# python main_taxiBJ.py
+
+cd ../Autoencoder
+python transfer_learning_evaluation.py
